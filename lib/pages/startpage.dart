fb3fbb0fd7446a3d07fd7d6e00587d7ec0a292b7
fireslayer60 <krishpandey2004@gmail.com>
2023-09-17 20:02:24 +0530

    vastu3

--- lib/pages/startpage.dart
+++ lib/pages/startpage.dart
@@ -8,14 +8,59 @@ class MyWidget extends StatelessWidget {
     return SafeArea(
         child: Scaffold(
       body: Container(
+        padding: EdgeInsets.only(bottom: 50),
         child: Center(
           child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-            Text(
+            const Image(
+              image: AssetImage("lib/images/image.png"),
+              height: 207,
+              width: 207,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const Text(
               "VASTU",
               style: TextStyle(
                   color: Color(0xFFE35629),
                   fontSize: 48,
-                  fontWeight: FontWeight.w500),
+                  fontWeight: FontWeight.w400),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              height: 60.0,
+              width: 265,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  gradient: LinearGradient(colors: [
+                    Color(0xFFE35629),
+                    Color(0xFFA6FF60).withOpacity(0.5)
+                  ])),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 15),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    TextButton(
+                      onPressed: () {},
+                      style: TextButton.styleFrom(
+                          backgroundColor: Colors.transparent,
+                          shadowColor: Colors.transparent),
+                      child: Text(
+                        'DIVE IN',
+                        style: TextStyle(color: Colors.white, fontSize: 18),
+                      ),
+                    ),
+                    Icon(
+                      Icons.arrow_forward,
+                      color: Colors.white,
+                      size: 30,
+                    )
+                  ],
+                ),
+              ),
             )
           ]),
         ),
